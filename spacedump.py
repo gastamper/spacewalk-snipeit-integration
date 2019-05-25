a45e3d3c8bffb31112643899b4cb98e65ca3ea87
Greg Stamper <gastamper@gmail.com>
2019-05-25 09:28:11 -0400

    Can now search Spacewalk for individual asset

--- spacedump.py
+++ spacedump.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python36
 import xmlrpc.client as xc
 import re, requests, configparser
-from sys import exit, exc_info
+from sys import exit, exc_info, argv
 
 config = configparser.ConfigParser()
 config['DEFAULT'] = {'SATELLITE_URL': "https://your_satellite_url",
@@ -13,6 +13,10 @@ SATELLITE_URL = config['DEFAULT']['SATELLITE_URL']
 SATELLITE_LOGIN = config['DEFAULT']['SATELLITE_LOGIN']
 SATELLITE_PASSWORD = config['DEFAULT']['SATELLITE_PASSWORD']
 
+if len(argv) is 1:
+    print("Syntax: %s <asset name>" % argv[0])
+    exit(1)
+
 #Populate a list of systems from Spacewalk
 with xc.Server(SATELLITE_URL, verbose=0) as client:
   try:
@@ -21,8 +25,12 @@ with xc.Server(SATELLITE_URL, verbose=0) as client:
   except:
     logger.error("Error connecting to Spacewalk: %s" % exc_info()[1])
     exit(1)
-system = query[0]
-if system:
+system = None
+for item in query:
+    if argv[1] == item["name"]: 
+        system = item
+        break
+if system is not None:
     for item in 'id','name','last_checkin':
         print("%s: %s" % (item, system[item]))
     print("CPU: ")
@@ -41,5 +49,8 @@ if system:
     print("DMI: ")
     for k,val in client.system.getDmi(key, system['id']).items():
         print("\t%s: %s" % (k, val))
-    exit(1)
+    exit(0)
     client.auth.logout(key)
+else:
+    print("No such system found in Spacewalk.")
+    exit(1)
